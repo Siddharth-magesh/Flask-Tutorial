2ddf4ca86c7729da6270e081a226beedd52fe164
Siddharth-magesh <siddharthmagesh007@gmail.com>
2024-07-11 06:53:05 +0530

    updates

--- database.sql
+++ database.sql
@@ -1,3 +1,11 @@
 create database mydatabase;
 
 use mydatabase;
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (id)
+);
